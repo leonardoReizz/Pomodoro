070d0adfc7ef2d27c3a4f98aeb2e265aef165f3d
Leonardo Reis <amarantee.reis@gmail.com>
2023-01-08 14:53:29 -0300

    add button in navbar to login route

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,28 @@
 import { Logo, Nav, NavbarContainer } from './styles';
 import { IoMdSettings } from 'react-icons/io';
 import { BiHistory } from 'react-icons/bi';
+import { AiOutlineUser } from 'react-icons/ai';
 import { useState } from 'react';
 import { SettingsModal } from './components/SettingsModal';
 import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 export function Navbar() {
   const [isOpenSettingsModal, setIsOpenSettingsModal] = useState<boolean>(false);
-
+  const navigate = useNavigate();
   function handleOpenSettingsModal() {
     setIsOpenSettingsModal(true);
+  }
+
+  function handleHistory() {
+    navigate('/history');
+  }
+  function handleLogin() {
+    navigate('/login');
   }
   const closeSettingsModal = useCallback(() => {
     setIsOpenSettingsModal(false);
   }, []);
+
   return (
     <>
       <SettingsModal isOpen={isOpenSettingsModal} onRequestClose={closeSettingsModal} />
@@ -28,8 +38,13 @@ export function Navbar() {
               </button>
             </li>
             <li>
-              <button>
+              <button type="button" onClick={handleHistory}>
                 <BiHistory /> History
+              </button>
+            </li>
+            <li>
+              <button type="button" onClick={handleLogin}>
+                <AiOutlineUser /> Login
               </button>
             </li>
           </ul>
