6306d1030582b8213f72d567e90d8349a93b5af1
Leonardo Reis <amarantee.reis@gmail.com>
2023-01-08 15:44:14 -0300

    feat: created settings modal

--- src/components/Navbar/components/SettingsModal/index.tsx
+++ src/components/Navbar/components/SettingsModal/index.tsx
@@ -1,14 +1,83 @@
 import ReactModal from 'react-modal';
-
+import { ButtonClose, Header, ModalContainer, Times } from './styles';
+import { GrFormClose } from 'react-icons/gr';
+import { useContext } from 'react';
+import { CyclesContext } from '@contexts/CyclesContext/CyclesContext';
 interface SettingsModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
 export function SettingsModal({ isOpen, onRequestClose }: SettingsModalProps) {
+  const {
+    changeTotalMinutesPomodoro,
+    changeTotalMinutesLongBreak,
+    changeTotalMinutesShortBreak,
+    totalMinutesPomodoro
+  } = useContext(CyclesContext);
+
+  function handleMinutesPomodoro(minutes: string) {
+    changeTotalMinutesPomodoro(Number(minutes));
+  }
+
+  function handleMinutesShortBreak(minutes: string) {
+    changeTotalMinutesShortBreak(Number(minutes));
+  }
+
+  function handleMinutesLongBreak(minutes: string) {
+    changeTotalMinutesLongBreak(Number(minutes));
+  }
+
   return (
-    <ReactModal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h3>OLA</h3>
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="reactModalOverlay"
+      className="reactModalContent">
+      <ModalContainer>
+        <Header>
+          <h3>TIMER SETTINGS</h3>
+        </Header>
+        <Times>
+          <div>
+            <h4>Pomodoro</h4>
+            <input
+              type="number"
+              step={5}
+              max={60}
+              min={5}
+              defaultValue={5}
+              onChange={(e) => handleMinutesPomodoro(e.target.value)}
+            />
+          </div>
+          <div>
+            <h4>Short Break</h4>
+            <input
+              type="number"
+              step={5}
+              max={60}
+              min={5}
+              defaultValue={totalMinutesPomodoro}
+              onChange={(e) => handleMinutesShortBreak(e.target.value)}
+            />
+          </div>
+          <div>
+            <h4>Long Break</h4>
+            <input
+              type="number"
+              step={5}
+              max={60}
+              min={5}
+              defaultValue={5}
+              onChange={(e) => handleMinutesLongBreak(e.target.value)}
+            />
+          </div>
+        </Times>
+
+        <ButtonClose type="button" onClick={onRequestClose}>
+          Close
+        </ButtonClose>
+      </ModalContainer>
     </ReactModal>
   );
 }
---
+++ src/components/Navbar/components/SettingsModal/styles.ts
@@ -0,0 +1,80 @@
+import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+  max-width: 700px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  gap: 2rem;
+  padding: 2rem 0;
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: space-around;
+  justify-content: space-between;
+  padding: 0 2rem;
+  position: relative;
+  &::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    width: 90%;
+    height: 1px;
+    background-color: ${(props) => props.theme['gray-600']};
+  }
+  h3 {
+    color: ${(props) => props.theme['gray-600']};
+  }
+
+  button {
+    color: red;
+    svg {
+      font-size: 1.3rem;
+      color: white;
+      color: ${(props) => props.theme['white']};
+    }
+  }
+`;
+
+export const Times = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: space-around;
+
+  width: 100%;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    input {
+      max-width: 80px;
+      height: 45px;
+      background: ${(props) => props.theme['gray-600']};
+      border: 0;
+      border-radius: 8px;
+      padding: 0 0.5rem;
+    }
+  }
+`;
+
+export const ButtonClose = styled.button`
+  background-color: ${(props) => props.theme['red-500']};
+  width: 300px;
+  height: 50px;
+
+  position: absolute;
+  bottom: 2rem;
+  border-radius: 8px;
+  transition: background-color 0.2s ease;
+  &:hover {
+    background-color: ${(props) => props.theme['red-700']};
+  }
+`;
