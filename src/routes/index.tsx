95453737e6cc676ca46d09be379ea4b4a34ab324
Leonardo Reis <amarantee.reis@gmail.com>
2023-01-08 14:50:01 -0300

    add login route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,19 @@
+/* eslint-disable react/jsx-no-undef */
 import { Route, Routes } from 'react-router-dom';
 import App from '../App';
 import { Home } from '@pages/Home';
+import { History } from '@pages/History';
 import { LayoutWithNavbar } from './LayoutWithNavbar';
+import { Login } from '@pages/Login';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route element={<LayoutWithNavbar />}>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
       </Route>
     </Routes>
   );
