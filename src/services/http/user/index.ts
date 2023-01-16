4cfb6056ee83733ad8b295c4869d52ee62326775
Leonardo Reis <amarantee.reis@gmail.com>
2023-01-16 07:03:01 -0300

    refactor: changed route of login

--- src/services/http/user/index.ts
+++ src/services/http/user/index.ts
@@ -2,13 +2,14 @@ import { api } from '../../api';
 import * as types from './types';
 
 export async function login(user: types.ILoginRequest): Promise<types.IUser> {
-  const login: types.IUser[] = await api
-    .post('/users/login', {
+  const login: types.IUser = await api
+    .post('/auth/login', {
       email: user.email,
       password: user.password
     })
     .then((result) => {
-      return result.data.message[0];
+      console.log(result.data);
+      return result.data.message;
     })
     .catch((error) => {
       console.log(error);
