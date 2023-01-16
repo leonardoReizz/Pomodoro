d473934a009955b51123dd403c5c818d3104234e
Leonardo Reis <amarantee.reis@gmail.com>
2023-01-16 07:01:56 -0300

    feat: add new contexts and setAppElement from react-modal

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
+import ReactModal from 'react-modal';
+import { ToastContainer } from 'react-toastify';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { CyclesContextProvider } from '@contexts/CyclesContext/CyclesContext';
 import { AppRoutes } from './routes';
-import { GlobalStyle } from './styles/global';
+import { UserContextProvider } from '@contexts/UserContext/UserContext';
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 import 'react-toastify/dist/ReactToastify.css';
+
+ReactModal.setAppElement('#root');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ToastContainer />
     <ThemeProvider theme={defaultTheme}>
-      <CyclesContextProvider>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-      </CyclesContextProvider>
+      <UserContextProvider>
+        <CyclesContextProvider>
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+        </CyclesContextProvider>
+      </UserContextProvider>
       <GlobalStyle />
     </ThemeProvider>
   </React.StrictMode>
