14243a03f82e31a7dd3c574a3e928b01afc81409
Leonardo Reis <amarantee.reis@gmail.com>
2023-02-22 22:36:28 -0300

    fix: syntax css

--- src/pages/Home/components/Header/styles.ts
+++ src/pages/Home/components/Header/styles.ts
@@ -1,20 +1,20 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const HeaderContainer = styled.div`
   display: flex;
   justify-content: center;
 
   .selected {
-    background-color: ${(props) => props.theme['gray-600']}
+    background-color: ${(props) => props.theme['gray-600']};
   }
 
   ul {
     display: flex;
-    gap: .5rem
+    gap: 0.5rem;
   }
 
   button {
-    padding: .5rem;
+    padding: 0.5rem;
     border-radius: 8px;
   }
-`
+`;
